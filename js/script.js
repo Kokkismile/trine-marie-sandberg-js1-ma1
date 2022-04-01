73e14030ce130f2e12a4a238827436d90e0aac0a
Trine Sandberg <trine.marie.sandberg@gmail.com>
2022-04-01 14:54:03 +0200

    ansvering question 3

--- js/script.js
+++ js/script.js
@@ -6,7 +6,13 @@ const cat = {
 };
 
 //Question 2
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
 
+//Question 3
+heading.style.fontSize = "2em";
+
+//Question 4
 
 const cats = [
     {
